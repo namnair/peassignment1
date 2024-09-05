237e0b5eb646fe13946cd5307de997b8a099ce30
namnair <93312156+namnair@users.noreply.github.com>
2024-09-05 23:22:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('welcomebtn').addEventListener('click', function() {
+    alert('Hello, welcome to my page!');
+});
+document.getElementById('profilepic').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = '#FFB6C1';
+});
+document.getElementById('profilepic').addEventListener('mouseout', function() {
+    document.body.style.backgroundColor = '	#DB7093 ';
+});
